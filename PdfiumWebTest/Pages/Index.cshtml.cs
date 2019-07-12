73146b086af833f5ea9cfe9b4d433e6510edf985
Thomas Mutton <tmutton@spacehive.com>
2019-07-12 10:58:00 +0100

    files

---
+++ PdfiumWebTest/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PDFiumSharp;
+
+namespace PdfiumWebTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // pdf extraction here
+            using (var doc = new PdfDocument("c:\\test.pdf"))
+            {
+                int i = 0;
+                foreach (var page in doc.Pages)
+                {
+                    using (var bitmap = new PDFiumBitmap((int)page.Width, (int)page.Height, true))
+                    using (var stream = new FileStream($"{i++}.bmp", FileMode.Create))
+                    {
+                        page.Render(bitmap);
+                        bitmap.Save(stream);
+                    }
+                }
+            }
+        }
+    }
+}
